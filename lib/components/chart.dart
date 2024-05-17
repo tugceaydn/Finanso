c1161b4b88b5848f9ef0895e1bf0d2da8092eced
Tugce <tugce.aydin54@gmail.com>
2024-05-17 17:41:16 +0300

    stock details api + styled list navigation + chart update

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -3,15 +3,8 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../core/app_themes.dart';
 
-class StockData {
-  StockData(this.date, this.price);
-
-  final String date;
-  final double price;
-}
-
 class Chart extends StatelessWidget {
-  final List<StockData> data;
+  final List<Map<String, dynamic>> data;
   final Color? color;
 
   const Chart({
@@ -20,24 +13,30 @@ class Chart extends StatelessWidget {
     this.color,
   });
 
+  @override
   Widget build(BuildContext context) {
     return SfCartesianChart(
       primaryXAxis: const CategoryAxis(
         isVisible: false,
         majorGridLines: MajorGridLines(width: 0),
       ),
-      primaryYAxis: const NumericAxis(
+      primaryYAxis: NumericAxis(
         isVisible: false,
-        initialVisibleMinimum: 987 - 200,
+        initialVisibleMinimum: data
+                .map((e) => e['close'] as double)
+                .reduce((value, element) => element < value ? element : value) -
+            10,
       ),
       legend: const Legend(isVisible: true),
       tooltipBehavior: TooltipBehavior(enable: true),
-      series: <CartesianSeries<StockData, String>>[
-        LineSeries<StockData, String>(
+      series: <CartesianSeries<Map<String, dynamic>, String>>[
+        LineSeries<Map<String, dynamic>, String>(
           color: color ?? primary,
           dataSource: data,
-          xValueMapper: (StockData sales, _) => sales.date,
-          yValueMapper: (StockData sales, _) => sales.price,
+          xValueMapper: (Map<String, dynamic> data, _) =>
+              data['date'] as String,
+          yValueMapper: (Map<String, dynamic> data, _) =>
+              data['close'] as double,
           isVisibleInLegend: false,
           name: '',
           dataLabelSettings: const DataLabelSettings(isVisible: false),
--- lib/components/styled_list.dart
+++ lib/components/styled_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stock_market/components/styled_text.dart';
 import 'package:stock_market/core/app_themes.dart';
+import 'package:stock_market/main.dart';
+import 'package:stock_market/pages/stock_details.dart';
 
 class StyledList extends StatelessWidget {
   final List<Map<String, dynamic>> stockDataList;
@@ -25,100 +27,110 @@ class StyledList extends StatelessWidget {
         final bool isFirstItem = index == 0;
         final bool isLastItem = index == stockDataList.length - 1;
 
-        return Container(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border(
-              bottom: BorderSide(
-                color: isLastItem
-                    ? Colors.transparent
-                    : const Color.fromRGBO(233, 233, 233, 1),
-                width: 1,
+        return InkWell(
+          onTap: () {
+            navigatorKey.currentState?.push(
+              MaterialPageRoute(
+                builder: (context) => StockDetails(symbol: stockData["symbol"]),
+              ),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border(
+                bottom: BorderSide(
+                  color: isLastItem
+                      ? Colors.transparent
+                      : const Color.fromRGBO(233, 233, 233, 1),
+                  width: 1,
+                ),
+              ),
+              borderRadius: BorderRadius.vertical(
+                top: isFirstItem ? const Radius.circular(8) : Radius.zero,
+                bottom: isLastItem ? const Radius.circular(8) : Radius.zero,
               ),
             ),
-            borderRadius: BorderRadius.vertical(
-              top: isFirstItem ? const Radius.circular(8) : Radius.zero,
-              bottom: isLastItem ? const Radius.circular(8) : Radius.zero,
-            ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ClipOval(
-                    child: Image.network(
-                      stockData['logo'],
-                      fit: BoxFit.fitWidth,
-                      width: 24.0,
-                      height: 24.0,
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      StyledText(
-                        text: '${stockData['symbol']}',
-                        type: 'body',
-                        color: textPrimary,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: Image.network(
+                        stockData['logo'],
+                        fit: BoxFit.fitWidth,
+                        width: 24.0,
+                        height: 24.0,
                       ),
-                      isCurrentPriceIncluded
-                          ? StyledText(
-                              text:
-                                  '\$${stockData['price']['current'].toStringAsFixed(2)}',
-                              type: 'functional',
-                            )
-                          : Container()
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: !onlySector
-                    ? [
+                    ),
+                    const SizedBox(width: 8),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
                         StyledText(
-                          text: stockData['invested'] > 0
-                              ? '\$${stockData['invested'].toStringAsFixed(2)}'
-                              : '-\$${(stockData['invested']).abs().toStringAsFixed(2)}',
+                          text: '${stockData['symbol']}',
                           type: 'body',
                           color: textPrimary,
                         ),
-                        Row(
-                          children: [
-                            StyledText(
-                              text: stockData['gain'] > 0
-                                  ? '+\$${stockData['gain'].toStringAsFixed(2)} '
-                                  : '-\$${(stockData['gain']).abs().toStringAsFixed(2)} ',
-                              type: 'functional',
-                              color:
-                                  stockData['gain'] > 0 ? greenSolid : redSolid,
-                            ),
-                            StyledText(
-                              text: stockData['gainPercent'] > 0
-                                  ? '(+${stockData['gainPercent'].toStringAsFixed(2)}%)'
-                                  : '(${stockData['gainPercent'].toStringAsFixed(2)}%)',
-                              type: 'functional',
-                              color: stockData['gainPercent'] > 0
-                                  ? greenSolid
-                                  : redSolid,
-                            ),
-                          ],
-                        ),
-                      ]
-                    : [
-                        StyledText(
-                          text: '${stockData['sector']}',
-                          type: 'body',
-                          color: textPrimary,
-                        ),
+                        isCurrentPriceIncluded
+                            ? StyledText(
+                                text:
+                                    '\$${stockData['price']['current'].toStringAsFixed(2)}',
+                                type: 'functional',
+                              )
+                            : Container()
                       ],
-              ),
-            ],
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: !onlySector
+                      ? [
+                          StyledText(
+                            text: stockData['invested'] > 0
+                                ? '\$${stockData['invested'].toStringAsFixed(2)}'
+                                : '-\$${(stockData['invested']).abs().toStringAsFixed(2)}',
+                            type: 'body',
+                            color: textPrimary,
+                          ),
+                          Row(
+                            children: [
+                              StyledText(
+                                text: stockData['gain'] > 0
+                                    ? '+\$${stockData['gain'].toStringAsFixed(2)} '
+                                    : '-\$${(stockData['gain']).abs().toStringAsFixed(2)} ',
+                                type: 'functional',
+                                color: stockData['gain'] > 0
+                                    ? greenSolid
+                                    : redSolid,
+                              ),
+                              StyledText(
+                                text: stockData['gainPercent'] > 0
+                                    ? '(+${stockData['gainPercent'].toStringAsFixed(2)}%)'
+                                    : '(${stockData['gainPercent'].toStringAsFixed(2)}%)',
+                                type: 'functional',
+                                color: stockData['gainPercent'] > 0
+                                    ? greenSolid
+                                    : redSolid,
+                              ),
+                            ],
+                          ),
+                        ]
+                      : [
+                          StyledText(
+                            text: '${stockData['sector']}',
+                            type: 'body',
+                            color: textPrimary,
+                          ),
+                        ],
+                ),
+              ],
+            ),
           ),
         );
       },
